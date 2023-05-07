b260b073e06ab0e01651ad71a9f89f438e564c8e
BlacIP <omotoyinbobade15@gmail.com>
2023-05-07 03:50:28 +0100

    Learning Sample Week 2 Done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Graphic Designer";
+        job1._company = "Kunslar Coms";
+        job1._startYear = 2019;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "XO By Lewa";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Boluwatife Adebiyi";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

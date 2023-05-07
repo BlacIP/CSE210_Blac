f7cb7e2a21e58a4b3ba8a4994d558722bef4c72c
BlacIP <omotoyinbobade15@gmail.com>
2023-05-07 06:40:38 +0100

    Prove Wk 3

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<JournalEntry> entries = new List<JournalEntry>();
+    private List<string> prompts = new List<string>();
+
+    public Journal()
+    {
+        // Initialize the prompts list with pre-written prompts
+        prompts.Add("Who was the most interesting person I interacted with today?");
+        prompts.Add("What was the best part of my day?");
+        prompts.Add("How did I see the hand of the Lord in my life today?");
+        prompts.Add("What was the strongest emotion I felt today?");
+        prompts.Add("If I had one thing I could do over today, what would it be?");
+    }
+
+    public void AddEntry(JournalEntry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (JournalEntry entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(prompts.Count);
+        return prompts[index];
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        entries.Clear();
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split('|');
+                    if (parts.Length == 3)
+                    {
+                        string date = parts[0];
+                        string prompt = parts[1];
+                        string response = parts[2];
+                        JournalEntry entry = new JournalEntry(date, prompt, response);
+                        entries.Add(entry);
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found: " + fileName);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error loading journal from file: " + ex.Message);
+        }
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (JournalEntry entry in entries)
+                {
+                    string line = $"{entry.Date}|{entry.Prompt}|{entry.Response}";
+                    writer.WriteLine(line);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error saving journal to file: " + ex.Message);
+        }
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,95 @@
 using System;
 
-class Program
+public class Program
 {
+    private static Journal journal = new Journal();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            Console.WriteLine("==== Welcome ====");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display your journal entries");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load an exiting jounal file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                WriteNewEntry();
+            }
+            else if (choice == "2")
+            {
+                DisplayJournal();
+            }
+            else if (choice == "3")
+            {
+                SaveJournal();
+            }
+            else if (choice == "4")
+            {
+                LoadJournal();
+            }
+            else if (choice == "5")
+            {
+                isRunning = false;
+                Console.WriteLine("Exiting the program...");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void WriteNewEntry()
+    {
+        string date = DateTime.Now.ToString("yyyy-MM-dd");
+        string prompt = journal.GetRandomPrompt();
+
+        Console.WriteLine($"Date: {date}");
+        Console.WriteLine($"Prompt: {prompt}");
+        Console.Write("Response: ");
+        string response = Console.ReadLine();
+
+        JournalEntry entry = new JournalEntry(date, prompt, response);
+        journal.AddEntry(entry);
+    }
+
+    static void DisplayJournal()
+    {
+        Console.WriteLine("==== Display Journal ====");
+        Console.Write("Enter the file name of the saved journal: ");
+        string fileName = Console.ReadLine();
+
+        journal.LoadFromFile(fileName);
+        journal.DisplayEntries();
+    }
+
+    static void SaveJournal()
+    {
+        Console.WriteLine("==== Save Journal ====");
+        Console.Write("Enter the file name to save the journal: ");
+        string fileName = Console.ReadLine();
+
+        journal.SaveToFile(fileName);
+        Console.WriteLine("Journal saved successfully.");
+    }
+
+    static void LoadJournal()
+    {
+        Console.WriteLine("==== Load Journal ====");
+        Console.Write("Enter the file name to load the journal: ");
+        string fileName = Console.ReadLine();
+
+        journal.LoadFromFile(fileName);
+        Console.WriteLine("Journal loaded successfully.");
     }
 }
